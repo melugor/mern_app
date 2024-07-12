d0f49137bcd57909e97a8bc2e60e2db04f4ca76b
melugor <melugor@gmail.com>
2024-07-12 11:45:46 -0500

    pag 62

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {API_VERSION} = require ("./constants")
+
+const app = express()
+
+// Importar rutas
+
+//Configurar Body Parse
+
+//Configurar Header HTTP - CORS
+
+//Configurar Rutas 
+
+module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose")
+const app = require("./app.js")
 const {DB_USER,DB_PASSWORD,DB_HOST,API_VERSION,IP_SERVER}= require("./constants.js")
 
 
-mongoose.set("strictQuery",false)
+const PORT = process.env.POST || 3000
+
+mongoose.set("strictQuery", false);
 
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
     (error) =>{
         if(error) throw error
-        console.log("La conexion al servidor ha sido existosa!");
+
+        app.listen(PORT, () => {
+            console.log("#############################")
+            console.log("###    MERN   API   REST  ###")
+            console.log("#############################")
+            console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}`)
+        }
+      )
     }
 )
